2b30c9f22d9063d5d4a9ace58cbbab68704314d1
oussama rajhi <oussama.rajhi@esprit.tn>
2020-07-31 13:27:02 +0200

    added flags and xml

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/xml"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
@@ -11,12 +13,33 @@ import (
 	links "github.com/oucema001/link"
 )
 
+const (
+	// A generic XML header suitable for use with the output of Marshal.
+	// This is not automatically added to any output of this package,
+	// it is provided as a convenience.
+	Header = `<?xml version="1.0" encoding="UTF-8"?>` + "\n"
+)
+
 func main() {
-	f := flag.String("url", "https://www.calhoun.io", "url to use for the sitemap")
+	urlFlag := flag.String("url", "https://www.calhoun.io", "url to use for the sitemap")
+	depth := flag.Int("depth", 2, "depth to traverse for the sitemap")
+	filename := flag.String("filename", "sitemap.xml", "file name to output sitemap to")
 	flag.Parse()
-	_ = f
-	//getLinks("https://www.calhoun.io")
-	siteTraverse(3, "https://www.calhoun.io")
+
+	l := siteTraverse(*depth, *urlFlag)
+
+	BuildSiteMapXML(*filename, l)
+}
+
+type URLSiteMap struct {
+	//URL string `xml:"url"`
+	XMLName xml.Name `xml:"urlset"`
+	XMLNS   string   `xml:"xmlns:prefix,attr"`
+	URL     []u      `xml:"url"`
+}
+
+type u struct {
+	Loc string `xml:"loc"`
 }
 
 func getLinks(urlStr string) []string {
@@ -80,11 +103,13 @@ func siteTraverse(depth int, urlStr string) []string {
 				continue
 			}
 			list := getLinks(k)
+
 			for _, v := range list {
-				if _, ok := q[v]; !ok {
-					nq[v] = struct{}{}
-					res[v] = struct{}{}
+				if _, ok := res[k]; ok {
+					continue
 				}
+				res[v] = struct{}{}
+				nq[v] = struct{}{}
 			}
 		}
 		i++
@@ -96,4 +121,33 @@ func siteTraverse(depth int, urlStr string) []string {
 		ret = append(ret, k)
 	}
 	return ret
+}
+
+func BuildSiteMapXML(filename string, l []string) {
+	m := make([]u, 0)
+	for _, v := range l {
+		if v == "" {
+			continue
+		}
+		k := u{
+			v,
+		}
+		m = append(m, k)
+	}
+	u := URLSiteMap{
+		XMLNS: "http://www.sitemaps.org/schemas/sitemap/0.9",
+		URL:   m,
+	}
+	//b, err := xml.Marshal(u)
+	var bz []byte
+	bz = []byte(xml.Header)
+	b, err := xml.MarshalIndent(u, "", " ")
+	if err != nil {
+		log.Println(err)
+	}
+	bz = append(bz, b...)
+	err = ioutil.WriteFile(filename, bz, 0644)
+	if err != nil {
+		log.Println(err)
+	}
 }
